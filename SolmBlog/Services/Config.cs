9f0ca75596d65ad8ab5dde95df55c99091ddc8a9
Max <bloodmailvol1@gmail.com>
2021-07-11 00:28:17 +0300

    Update appsettings file
    and add config.cs file

---
+++ SolmBlog/Services/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolmBlog.Services
+{
+    public class Config
+    {
+        public static string connectionString { get; set; }
+        public static string siteName { get; set; }
+        public static string contactMail { get; set; }
+    }
+}
