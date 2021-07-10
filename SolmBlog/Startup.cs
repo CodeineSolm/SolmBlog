1c36749c0e060c5a44a2caf2136f18541ce2e226
Max <bloodmailvol1@gmail.com>
2021-07-11 00:37:47 +0300

    Add config to startup.cs and bind it
    also add title from config in metatagsPartial

--- SolmBlog/Startup.cs
+++ SolmBlog/Startup.cs
@@ -4,13 +4,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SolmBlog.Services;
 
 namespace SolmBlog
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.Bind("Project", new Config());
             services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
