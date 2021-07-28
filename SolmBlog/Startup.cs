6a10c10bbb741295a57224eb87b90c8678ec4ab8
Max <bloodmailvol1@gmail.com>
2021-07-28 21:52:48 +0300

    Add all services and configures to startup.cs class

--- SolmBlog/Startup.cs
+++ SolmBlog/Startup.cs
@@ -6,6 +6,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using SolmBlog.Services;
+using SolmBlog.Domain.Repositories.Abstract;
+using SolmBlog.Domain.Repositories.EntityFramework;
+using SolmBlog.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace SolmBlog
 {
@@ -21,6 +26,29 @@ namespace SolmBlog
         public void ConfigureServices(IServiceCollection services)
         {
             Configuration.Bind("Project", new Config());
+            services.AddTransient<IArticleRepository, EFArticleRepository>();
+            services.AddTransient<DataManager>();
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "SolmBlogAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
             services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
@@ -31,8 +59,11 @@ namespace SolmBlog
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
             app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
